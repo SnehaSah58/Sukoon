6cf5dfe5cff288be243c074665ece797f13d5cf8
Sneha Sah <snehasah0307@gmail.com>
2026-09-21 12:31:30 +0530

    Initial Sukoon project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="landing-page">
+      <main className="welcome-content">
+
+        <p className="welcome-label">WELCOME TO</p>
+
+        <h1>sukoon.</h1>
+
+        <p className="welcome-text">
+          A little corner of the internet
+          <br />
+          where you don't have to be anything.
+        </p>
+
+        <button className="enter-button">
+          🎧 Enter Sukoon
+        </button>
+
+        <button className="share-button">
+          ↗ Share Sukoon
+        </button>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
