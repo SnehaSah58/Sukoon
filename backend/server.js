6ee491a4a02f571982a9a71fe6fa033403224777
Sneha Sah <snehasah0307@gmail.com>
2026-09-23 17:46:48 +0530

    Refactor server.js to improve MongoDB connection handling and restructure middleware setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
+const dns = require("dns");
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+dns.setServers(["8.8.8.8"]);
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+// Middleware
+app.use(express.json());
 
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully ");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error.message);
+  });
+
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Sukoon backend is running 🌙");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
